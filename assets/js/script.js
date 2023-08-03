702ba4c200a56da0ef3a7db46f809e3637fb4f8d
Daniel Leon <danirl.99.117@gmail.com>
2023-08-03 17:26:37 -0600

    Cambio de color

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,9 @@ console.log("Pagina en construccion");
 function clickPatricio() {
     alert ("La mayonesa es un instrumento")
 }
+
+function changeColor( elementHtml, color ) {
+console.log(elementHtml); 
+console.log(color);
+elementHtml.style.color = color;
+}
